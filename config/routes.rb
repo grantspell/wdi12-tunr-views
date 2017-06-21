cfedb791dc7445bda5f4dac9fb80015d7d9f8653
king0120 <king0120@gmail.com>
2017-06-21 10:22:50 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :artists
+  get '/artists', to: 'artists#index'
+  get '/artists/new', to: 'artists#new'
+  post '/artists', to: 'artists#create'
+  get '/artists/:id', to: 'artists#show'
+  get '/artists/:id/edit', to: 'artists#edit'
+  put '/artists/:id', to: 'artists#update'
+  delete '/artists/:id', to: 'artists#destroy'
 end
