0554c8168e739f6a70a8c3e0b4f377f876b8a0c3
king0120 <king0120@gmail.com>
2017-10-27 16:51:25 -0400

    added some comments

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,48 +13,30 @@ class SongsController < ApplicationController
     @song = @artist.songs.new
   end
 
-  # GET /songs/1/edit
   def edit
   end
 
-  # POST /songs
-  # POST /songs.json
   def create
     @song = @artist.songs.new(song_params)
-
-    respond_to do |format|
-      if @song.save
-        format.html { redirect_to @song, notice: 'Song was successfully created.' }
-        format.json { render :show, status: :created, location: @song }
-      else
-        format.html { render :new }
-        format.json { render json: @song.errors, status: :unprocessable_entity }
-      end
+    
+    if @song.save
+      redirect_to artist_song_path(@artist, @song)
+    else
+      redirect_to :back 
     end
   end
 
-  # PATCH/PUT /songs/1
-  # PATCH/PUT /songs/1.json
   def update
-    respond_to do |format|
-      if @song.update(song_params)
-        format.html { redirect_to @song, notice: 'Song was successfully updated.' }
-        format.json { render :show, status: :ok, location: @song }
-      else
-        format.html { render :edit }
-        format.json { render json: @song.errors, status: :unprocessable_entity }
-      end
+    if @song.update(song_params)
+      redirect_to artist_song_path(@artist,@song)
+    else
+      redirect_to :back 
     end
   end
 
-  # DELETE /songs/1
-  # DELETE /songs/1.json
   def destroy
     @song.destroy
-    respond_to do |format|
-      format.html { redirect_to songs_url, notice: 'Song was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to artist_path(@artist)
   end
 
   private
@@ -62,6 +44,7 @@ class SongsController < ApplicationController
     def set_artist
       @artist = Artist.find(params[:artist_id])
     end
+    
     # Use callbacks to share common setup or constraints between actions.
     def set_song
       @song = @artist.songs.find(params[:id])
@@ -69,6 +52,6 @@ class SongsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
-      params.fetch(:song, {})
+      params.require(:song).permit(:title, :album, :preview_url)
     end
 end
